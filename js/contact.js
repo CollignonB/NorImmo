8de22b49b2af2791bc773a1467afaafed5b931e3
Baptiste COLLIGNON <collignon@CDI>
2020-08-21 09:19:11 +0200

    graphique interface creation

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+let lastnameInput = document.getElementById("lastnameInput");
+let firstnameInput = document.getElementById("firstnameInput");
+
+
+// Test si il y a des nombres dans la chaine de caratère
+function hasNumber (myString){
+    return /\d/.test(myString);
+}
+
+
